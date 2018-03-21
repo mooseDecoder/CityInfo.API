c222c8ca9676419f2dc15cae6bbd277d5b754263
Moose Decoder <moosedecoder@outlook.com>
2018-03-20 23:01:29 -0400

    more

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -11,6 +11,8 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,10 +23,13 @@ namespace CityInfo.API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+
         }
     }
 }
